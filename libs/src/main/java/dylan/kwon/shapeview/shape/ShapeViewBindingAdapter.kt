a9ebe0c449b1e777957c46363e5628cff3fb3285
Dylan Kwon <dylan.kwon98@gmail.com>
2022-06-29 13:47:56 +0900

    Shadow dataBinding 추가.

--- libs/src/main/java/dylan/kwon/shapeview/shape/ShapeViewBindingAdapter.kt
+++ libs/src/main/java/dylan/kwon/shapeview/shape/ShapeViewBindingAdapter.kt
@@ -9,5 +9,15 @@ import androidx.databinding.BindingMethods
         attribute = "solidColor",
         method = "setShapeColor"
     ),
+    BindingMethod(
+        type = ShapeView::class,
+        attribute = "shadow_x_offset",
+        method = "setShadowXOffset"
+    ),
+    BindingMethod(
+        type = ShapeView::class,
+        attribute = "shadow_y_offset",
+        method = "setShadowYOffset"
+    ),
 )
 object ShapeViewBindingAdapter
