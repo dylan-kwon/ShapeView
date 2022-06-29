9f432b674734325e6554c1520a5bcb3e546b48cd
Dylan Kwon <dylan.kwon98@gmail.com>
2022-06-29 13:41:44 +0900

    ShadowView setter 추가.

--- libs/src/main/java/dylan/kwon/shapeview/shadow/ShadowView.kt
+++ libs/src/main/java/dylan/kwon/shapeview/shadow/ShadowView.kt
@@ -7,5 +7,28 @@ interface ShadowView {
      */
     val shadowDelegate: ShadowViewDelegate
 
+    fun setShadowColor(shadowColor: Int) {
+        shadowDelegate.shadowColor = shadowColor
+    }
+
+    fun setShadowBlur(shadowBlur: Float) {
+        shadowDelegate.shadowBlur = shadowBlur
+    }
+
+    fun setShadowSpread(shadowSpread: Float) {
+        shadowDelegate.shadowSpread = shadowSpread
+    }
+
+    fun setShadowInset(shadowInset: Boolean) {
+        shadowDelegate.shadowInset = shadowInset
+    }
+
+    fun setShadowXOffset(xOffset: Float) {
+        shadowDelegate.shadowXOffset = xOffset
+    }
+
+    fun setShadowYOffset(yOffset: Float) {
+        shadowDelegate.shadowYOffset = yOffset
+    }
 
 }
